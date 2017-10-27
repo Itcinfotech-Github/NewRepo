fa84d9c1ed5817f1b6245764f0ce26ca40a0459f
22706 <Itcinfotech-Github>
2017-10-27 13:26:56 +0530

    first committ

---
+++ Github/src/test/java/Demo/Github/GithubDemo.java
@@ -0,0 +1,12 @@
+package Demo.Github;
+
+public class GithubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello git");
+		System.out.println("Hello git");
+	}
+
+}
